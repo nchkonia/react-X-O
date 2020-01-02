44e557b5bc335f1c3ad9f027e9ec65346b762638
nchkonia <nickchkonia96@gmail.com>
2020-01-02 15:01:43 -0600

    clened up for merge

--- src/index.js
+++ src/index.js
@@ -23,36 +23,27 @@ function Square({value, onClick}) {
 // main driver
 function Game (){
   // Game state hooks
-  const [squares, setSquares] = useState(Array(9).fill(null)); // init as empty
-  const [xIsNext, setXIsNext] = useState(true);               // bool switched on each render
-  const [gameStatus, setGameStatus] = useState('Next Player: X')           // recalculated on every render
+  const [squares, setSquares] = useState(Array(9).fill(null)); 
+  const [xIsNext, setXIsNext] = useState(true);               
+  const [gameStatus, setGameStatus] = useState('Next Player: X')           
 
   // history tracking
-  const [stepNumber, setStepNumber] = useState(0);            // stepNumber incremented on each render
-  const [history, setHistory] = useState([squares]);          // array of arrays
+  const [stepNumber, setStepNumber] = useState(0);            
+  const [history, setHistory] = useState([squares]);          
   const [currentSquares, setCurrentSquares] = useState(history[stepNumber]);
 
   // jump to int #step
   const jumpTo = (step) => {
     setStepNumber(step);
-    // note: same issue as with renderSquare.onClick: only updates on second render
-    // x is Next on even turns
-    console.log(`step: ${step}`);
-    // setXIsNext((step % 2) === 0);
-    console.log(`is x next when jumped? ${xIsNext}`)
     setSquares(history[step]);
-    // setCurrentSquares(squares);
   }
 
   useEffect(() => {
     setXIsNext((stepNumber%2) === 0);
-    console.log(`in effect hook, is X next? ${xIsNext}`);
-    // setGameStatus('Next Player: ' + (xIsNext? 'X' : 'O'));
     const winner = calculateWinner(squares);
     if (winner){
       setGameStatus('Winner: ' + winner)
     }
-    
     else{
       if (stepNumber === 9){
         setGameStatus("Draw");
@@ -61,14 +52,13 @@ function Game (){
         setGameStatus('Next Player: ' + (xIsNext? 'X' : 'O'));
       }
     }
-    // NOTE: add calculateWinner logic here, as it is a side-effect of rendering
-    // setHistory(history.slice(0,stepNumber));
-  })
+  }, [stepNumber, squares, xIsNext])
 
+
+  // maps step # and move data from history to ordered list buttons
   const moves = history.map((step, move) => {
     const desc = move ? 
       'Move #: ' + move : 'Game start';
-    // jump to (re-render) each move on click, each move stored in ordered list
     return (
       <li key={move}>
         <button onClick = {() => jumpTo(move)}>{desc}</button>;
@@ -78,46 +68,29 @@ function Game (){
 
 
 
-  // also updates square state info
+  // each square re-rendered on click
   const renderSquare = (i) => {
     return (
       <Square 
         value={squares[i]}  
-        // driver of state change
+        // main driver of state change
         onClick={() => {
-          // update squares when stepped back in history
           setSquares(currentSquares);
-          console.log(`current squares: ${currentSquares}`)
           if (calculateWinner(squares) || squares[i]){
-            console.log('already placed');
             return
           }
 
-          // WOW, seems to be CHANGING THE PREVIOUS MOVE TO THE CURRENT PLAYER'S VALUE
           const newSquares = squares.slice();
-          console.log(newSquares[i]);
           newSquares[i] = xIsNext ? 'X' : 'O'; 
-          console.log(newSquares[i]);
 
           setSquares(newSquares);
           setXIsNext(xIsNext? false : true);
-          console.log(`new squares: ${newSquares}`);
-          console.log(`squares: ${squares}`);
-          console.log(`is x next? ${xIsNext}`);
-          console.log('\n');
 
           const newStepNumber = stepNumber+1;
           setStepNumber(newStepNumber);
-          console.log(`stepNumber: ${stepNumber}`);
-          console.log(`newStepNumber: ${newStepNumber}`);
-          console.log('\n');
 
-
-          // this ONLY adds to history,
-          // const newHistory = history.concat([newSquares]);
-          // const newHistory = (history.slice(0, stepNumber)).concat([newSquares]);
+          // writing and rewriting history
           let newHistory;
-          // bandaid solution to rewriting history
           if (newStepNumber < history.length){
             newHistory = history.slice(0,newStepNumber).concat([newSquares]);
           }
@@ -125,33 +98,8 @@ function Game (){
             newHistory = history.concat([newSquares]);
           }
           
-          // const newHistory = history.slice(0, newStepNumber+1);
-
           setHistory(newHistory);
-
-          console.log('history');
-          console.log(history)
-          console.log('newHistory');
-          console.log(newHistory);
-          console.log('\n');
-
-          
           setCurrentSquares(newHistory[newStepNumber]);
-          console.log(`currentSquares: ${currentSquares}`);
-
-          // const winner = calculateWinner(newSquares);
-          // if (winner){
-          //   setGameStatus('Winner: ' + winner)
-          // }
-          
-          // else{
-          //   if (newStepNumber === 9){
-          //     setGameStatus("Draw");
-          //   }
-          //   else{
-          //     setGameStatus('Next Player: ' + (xIsNext? 'O' : 'X'));
-          //   }
-          // }
         }}
       />
     );
@@ -192,7 +140,6 @@ function Game (){
 // checks values in squares against these combinations
 // to determine a winner, if any
 function calculateWinner(squares){
-  console.log(`squares: ${squares}`);
   const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -207,7 +154,6 @@ function calculateWinner(squares){
     const [a, b, c] = lines[i];
     // if squares[a] exists and any winning combination is found 
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        console.log('WINNER')
         return squares[a];
     }
   }
